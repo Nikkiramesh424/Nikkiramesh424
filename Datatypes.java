307a0874022225f960a0862e7ab8d4aea135c51b
Nikshitha ramesh <160321766+Nikkiramesh424@users.noreply.github.com>
2024-08-08 12:24:34 +0530

    Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Datatypes
+{
+       static byte b;
+       static boolean bool;
+       static char ch;
+       static int i;
+public static void main(String args[])
+{
+      System.out.println("the default value of byte is"+b);
+      System.out.println("the default value of boolean is"+bool);
+      System.out.println("the default value of char is"+ch);
+      System.out.println("the default value of int is"+i);
+}
+} 
