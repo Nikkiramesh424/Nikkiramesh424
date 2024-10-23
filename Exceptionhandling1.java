bb21054baa7bbdbac8ae458cadbebfd8602d7b17
Nikshitha ramesh <160321766+Nikkiramesh424@users.noreply.github.com>
2024-10-23 14:25:03 +0530

    Exceptionhandling1.java

---
+++ Exceptionhandling1.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+class ABC
+{
+    public static void main(String args[])
+    {
+    try
+    {
+         int arr[]=new int[5];
+         System.out.println(arr[10]);
+    }
+    catch(ArithmeticException ae)
+    {
+          System.out.println("Arithmetic Exception");
+    }
+    catch(ArrayIndexOutOfBoundsException aiobe)
+    {
+          System.out.println("Out of Bounds Exception");
+    }
+    catch(Exception e)
+    {
+          System.out.println(e);
+    }
+   }
+  }
